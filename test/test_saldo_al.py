c0c2c47e0b647076a991b50ca62301e2e6834f5f
Omar Rampado <rampado.omar@gmail.com>
2019-05-24 18:15:52 +0200

    Refactoring. Add transaction package

--- test/test_saldo_al.py
+++ test/test_saldo_al.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime
 import pandas as pd
-from EstrattoConto import EstrattoConto
+from transaction import EstrattoConto
 
 def mock_loader(data):
 	columns = ['Data contabile', 'Causale', 'Descrizione operazione', 'Importo']
@@ -14,32 +14,29 @@ def mock_loader(data):
 
 class TestSaldoAl(unittest.TestCase):
 
-	def base_loader(self):
-		def _loader(f):
-			return mock_loader([['01/01/2019', 'Ricarica Tel.', 'nulla', -10.0],
-								['02/01/2019', 'Carta di credito.', 'nulla', -5.0],
-								['03/01/2019', 'ACCREDITO BONIFICO', 'nulla', 18.0]])
-		return _loader
+	test_df = mock_loader([['01/01/2019', 'Ricarica Tel.', 'nulla', -10.0],
+							['02/01/2019', 'Carta di credito.', 'nulla', -5.0],
+							['03/01/2019', 'ACCREDITO BONIFICO', 'nulla', 18.0]])
 
 	def test_saldo_al_before(self):
-		estratto = EstrattoConto('test', loader=self.base_loader())
-		self.assertEquals(estratto.saldo_al(datetime.strptime('31/12/2018','%d/%m/%Y')), 0.0)
+		estratto = EstrattoConto(self.test_df)
+		self.assertEqual(estratto.saldo_al(datetime.strptime('31/12/2018','%d/%m/%Y')), 0.0)
 
 	def test_saldo_al_after(self):
-		estratto = EstrattoConto('test', loader=self.base_loader())
-		self.assertEquals(estratto.saldo_al(datetime.strptime('20/01/2019','%d/%m/%Y')), 3.0)
+		estratto = EstrattoConto(self.test_df)
+		self.assertEqual(estratto.saldo_al(datetime.strptime('20/01/2019','%d/%m/%Y')), 3.0)
 
 	def test_saldo_al_inf(self):
-		estratto = EstrattoConto('test', loader=self.base_loader())
-		self.assertEquals(estratto.saldo_al(datetime.strptime('01/01/2019','%d/%m/%Y')), -10.0)
+		estratto = EstrattoConto(self.test_df)
+		self.assertEqual(estratto.saldo_al(datetime.strptime('01/01/2019','%d/%m/%Y')), -10.0)
 
 	def test_saldo_al_up(self):
-		estratto = EstrattoConto('test', loader=self.base_loader())
-		self.assertEquals(estratto.saldo_al(datetime.strptime('03/01/2019','%d/%m/%Y')), 3.0)
+		estratto = EstrattoConto(self.test_df)
+		self.assertEqual(estratto.saldo_al(datetime.strptime('03/01/2019','%d/%m/%Y')), 3.0)
 
 	def test_saldo_al_mid(self):
-		estratto = EstrattoConto('test', loader=self.base_loader())
-		self.assertEquals(estratto.saldo_al(datetime.strptime('02/01/2019','%d/%m/%Y')), -15.0)
+		estratto = EstrattoConto(self.test_df)
+		self.assertEqual(estratto.saldo_al(datetime.strptime('02/01/2019','%d/%m/%Y')), -15.0)
 
 if __name__ == '__main__':
 	unittest.main()
