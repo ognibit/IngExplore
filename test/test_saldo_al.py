d15a0effc729a893a5f221d8014bd4c679689485
Omar Rampado <rampado.omar@gmail.com>
2019-05-26 10:26:21 +0200

    Add giroconto tests

--- test/test_saldo_al.py
+++ test/test_saldo_al.py
@@ -14,29 +14,38 @@ def mock_loader(data):
 
 class TestSaldoAl(unittest.TestCase):
 
-	test_df = mock_loader([['01/01/2019', 'Ricarica Tel.', 'nulla', -10.0],
+	estratto = EstrattoConto(mock_loader([['01/01/2019', 'Ricarica Tel.', 'nulla', -10.0],
 							['02/01/2019', 'Carta di credito.', 'nulla', -5.0],
-							['03/01/2019', 'ACCREDITO BONIFICO', 'nulla', 18.0]])
+							['03/01/2019', 'ACCREDITO BONIFICO', 'nulla', 18.0]]))
 
-	def test_saldo_al_before(self):
-		estratto = EstrattoConto(self.test_df)
-		self.assertEqual(estratto.saldo_al(datetime.strptime('31/12/2018','%d/%m/%Y')), 0.0)
+	movimenti_giro = mock_loader([['01/01/2019', 'Ricarica Tel.', 'nulla', -10.0],      # -10
+							['03/01/2019', 'ACCREDITO BONIFICO', 'nulla', 20.0],      #  10
+							['04/01/2019', 'GIRO DA MIEI CONTI', 'nulla', 1000.0],    # 1010
+							['05/01/2019', 'GIRO VERSO MIEI CONTI', 'nulla', -500.0], # 510
+							['05/01/2019', 'VS.DISPOSIZIONE', 'nulla', -10.0]])       # 500
+
+	def test_saldo_al_before(self):		
+		self.assertEqual(self.estratto.saldo_al(datetime.strptime('31/12/2018','%d/%m/%Y')), 0.0)
 
 	def test_saldo_al_after(self):
-		estratto = EstrattoConto(self.test_df)
-		self.assertEqual(estratto.saldo_al(datetime.strptime('20/01/2019','%d/%m/%Y')), 3.0)
+		self.assertEqual(self.estratto.saldo_al(datetime.strptime('20/01/2019','%d/%m/%Y')), 3.0)
 
 	def test_saldo_al_inf(self):
-		estratto = EstrattoConto(self.test_df)
-		self.assertEqual(estratto.saldo_al(datetime.strptime('01/01/2019','%d/%m/%Y')), -10.0)
+		self.assertEqual(self.estratto.saldo_al(datetime.strptime('01/01/2019','%d/%m/%Y')), -10.0)
 
 	def test_saldo_al_up(self):
-		estratto = EstrattoConto(self.test_df)
-		self.assertEqual(estratto.saldo_al(datetime.strptime('03/01/2019','%d/%m/%Y')), 3.0)
+		self.assertEqual(self.estratto.saldo_al(datetime.strptime('03/01/2019','%d/%m/%Y')), 3.0)
 
 	def test_saldo_al_mid(self):
-		estratto = EstrattoConto(self.test_df)
-		self.assertEqual(estratto.saldo_al(datetime.strptime('02/01/2019','%d/%m/%Y')), -15.0)
+		self.assertEqual(self.estratto.saldo_al(datetime.strptime('02/01/2019','%d/%m/%Y')), -15.0)
+
+	def test_saldo_al_with_giroconto(self):
+		estratto_giro = EstrattoConto(self.movimenti_giro, giroconti=True)
+		self.assertEqual(estratto_giro.saldo_al(datetime.strptime('06/01/2019','%d/%m/%Y')), 500)
+
+	def test_saldo_al_without_giroconto(self):
+		estratto_giro = EstrattoConto(self.movimenti_giro, giroconti=False)
+		self.assertEqual(estratto_giro.saldo_al(datetime.strptime('06/01/2019','%d/%m/%Y')), 0)
 
 if __name__ == '__main__':
 	unittest.main()
